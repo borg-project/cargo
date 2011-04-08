34e8cced1fb7c8263520a994f4fca85a69f5621e
Bryan Silverthorn <bcs@cargo-cult.org>
2011-04-08 11:21:37 -0500

    Finish build process cleanup.

--- src/cmake/FindNumpy.cmake
+++ src/cmake/FindNumpy.cmake
@@ -6,7 +6,7 @@ if(NOT NUMPY_INCLUDE_DIRS)
     execute_process(
         COMMAND
             "python"
-            "${PROJECT_SOURCE_DIR}/scripts/echo_numpy_path.py"
+            "${PROJECT_SOURCE_DIR}/src/scripts/echo_numpy_path.py"
         OUTPUT_VARIABLE NUMPY_FOUND_PATH
         ERROR_VARIABLE NUMPY_FOUND_PATH
         RESULT_VARIABLE ECHO_NUMPY_PATH_FAILED
